8bcc1ba048acb41a9c9db2bdfcb0007672982768
Victor García Fernández <victorgf2011@gmail.com>
2024-06-04 09:50:15 +0200

    fix: connection on android emulator

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/foundation.dart';
 
 Map<String, String> config = {
-  'backend_url':
-      kDebugMode ? 'http://localhost:5482' : 'https://stash.tortitas.eu',
+  'backend_url': kDebugMode
+      // ? 'http://localhost:5482'
+      ? 'http://10.0.2.2:5482'
+      : 'https://stash.tortitas.eu',
 };
